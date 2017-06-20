54bbb1266f2d5bf4ae6f8a70b8d3e3be18b2a471
Oliver Crawford <ojc37@cam.ac.uk>
2017-06-20 01:38:38 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='PDFRevisionApp',
+    version='1',
+    packages=[''],
+    url='https://github.com/ocrawford555/PDFRevisionApp',
+    license='',
+    author='Oliver Crawford',
+    author_email='o.crawford@hotmail.co.uk',
+    description='See Github for more details: https://github.com/ocrawford555/PDFRevisionApp', requires=['PyPDF2',
+                                                                                                         'flask']
+)
